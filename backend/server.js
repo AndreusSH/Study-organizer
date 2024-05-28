d7bd1cdb296ff6b6c9e27a6ab8fb5e33e3514806
AndreusSH <andreamammoliti@yahoo.it>
2024-05-28 16:55:40 +0200

    changes necessary to set up a server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log('server is listening')
+const dotenv = require('dotenv'); // Import and configure dotenv
+const express = require('express'); // Import express
+const http = require('http'); // Import http module
+
+dotenv.config()
+const port = process.env.PORT || 5000
+const app = express()
+const server = http.createServer(app)
+
+
+//Start the server
+server.listen(port, ()=> {
+    console.log(`Server was started at PORT ${port}`)
+})
