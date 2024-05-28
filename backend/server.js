060680cc0378c432c8280083051b5a5bf5521a54
AndreusSH <andreamammoliti@yahoo.it>
2024-05-28 22:49:34 +0200

    adding functionalities

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
 const dotenv = require('dotenv'); // Import and configure dotenv
 const express = require('express'); // Import express
-const http = require('http'); // Import http module
+const cors = require('cors');
+const adminRoutes = require('./routes/adminRoutes.js')
 
 dotenv.config()
 const port = process.env.PORT || 5000
 const app = express()
 //const server = http.createServer(app)
 
+//allow cors for local frontend 
+const corsOptions = {
+    origin: 'http://localhost:3000', // Allow only this domain to access your API
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify which HTTP methods are allowed
+    credentials: true, // Allow cookies to be sent
+    optionsSuccessStatus: 204 // Some legacy browsers choke on 204
+};
+// Middleware for parsing JSON and URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+app.use('/api/users', adminRoutes);
 //message for the home page
 app.get('/', (req, res) => {
     res.send('This is the home page')
